ea6eafc72160cc72339af0e3fc8b4faf4b6614e1
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-11-22 09:38:41 -0300

    exportação de varias const no código modular

--- 04-codigo-modular.js
+++ 04-codigo-modular.js
@@ -1,6 +1,8 @@
 // 04-codigo-modular
 
-import pessoa from "./modulos/dados.js";
+import { pessoa } from "./modulos/dados.js";
+
+//import pessoa from "./modulos/dados.js"; --> assim quando só tem um const no arquivo dados.js
 
 console.log(pessoa);
 
--- modulos/dados.js
+++ modulos/dados.js
@@ -4,4 +4,16 @@ const pessoa = {
     cidade:'São Paulo'
 };
 
-export default pessoa;
+
+const livro ={
+    titulo: "O Senhor dos Anéis ",
+    volume: "As Duas Torres",
+    ano: 1954
+};
+
+/* Se for exportação  de somente UM recurso usamos  export default nomeDoRecurso*/
+//export default pessoa; quando só tem uma const
+
+
+/* Se for exportação de VÁRIOS recursos use somente export {entre chaves os recursos} tire o  default */
+export  { pessoa, livro };
