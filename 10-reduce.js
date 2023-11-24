239c3b87c23186086a1896b9b19b0de87c108ca4
Aline Teixeira <alinemachadoteixeira@gmail.com>
2023-11-24 11:34:01 -0300

    desafio

--- 10-reduce.js
+++ 10-reduce.js
@@ -1,3 +1,6 @@
+
+import cursos from "./modulos/cursos.js";
+
 /* Reduce (reduzir, redução) 
 Passa por elementos de um array (usando uma função callback com parâmetro com valor inicial) e retorna um unico valor/resultado. Pode ser útil para operações matemática que geram um único resultado.*/
 
@@ -12,7 +15,10 @@ console.log(total);
 // Sem reduce 
 let acumuladora1 = 0;
 for( let i = 0; i < valores.length; i++ ){
-    acumuladora1 = acumuladora1 + valores[i];
+    //acumuladora1 = acumuladora1 + valores[i]; igual o de baixo 
+    acumuladora1 += valores[i];
+
+    //+= operador soma e atribui
 };
 
 console.log(acumuladora1);
@@ -20,9 +26,37 @@ console.log(acumuladora1);
 // Sem reduce e com for/of
 let acumuladora2 = 0;
 for( let valor of valores ){
-    acumuladora2 = acumuladora2 + valor;
+    //acumuladora2 = acumuladora2 + valor; igual o de baixo 
+    acumuladora2 +=  valor;
 };
 
 console.log(acumuladora2);
 
 console.log("------------------");
+
+
+
+// const valores = [10, 5, 50, 200, 1000];
+// const total = valores.reduce( (acumulador, valor) => {
+//     return acumulador + valor
+// }, 0 );
+// console.log(valores);
+// console.log(total);
+
+/* Exercicio: faça a soma dos preços de todos os cursos  */
+
+const preco = cursos.reduce( (acumulador, curso) => {
+    return acumulador + curso.preco
+}, 0 );
+
+console.log(preco);
+
+
+/* Desafio: faça a soma dos preços  dos cursos de front e back end */
+
+
+const cursosDesafio1 = cursos
+                               .filter(curso => curso.categoria == "Front-End" || curso.categoria == "Back-End" )
+                               .reduce( (acumulador, curso) => {return acumulador + curso.preco }, 0 );  
+
+console.log(cursosDesafio1);                         
